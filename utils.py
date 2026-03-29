aaf63c77f62e0916f2bfa29c6e7d74c0332705fa
Derek Chen <derekjchen@proton.me>
2026-03-29 13:03:40 -0400

    fix: docstrings

--- utils.py
+++ utils.py
@@ -4,17 +4,35 @@ import rtc
 
 
 def timestamp():
-    """Return timestamp of ISO 8601 format using onboard RTC."""
+    """Return timestamp of ISO 8601 format using onboard RTC.
+
+    Returns:
+        str: ISO 8601 timestamp.
+    """
     r = rtc.RTC()
     t = r.datetime
 
-    ts = f"{t.tm_year:04d}-{t.tm_mon:02d}-{t.tm_mday:02d}T{t.tm_hour:02d}:{t.tm_min:02d}:{t.tm_sec:02d}"
+    ts = (
+        f"{t.tm_year:04d}-{t.tm_mon:02d}-{t.tm_mday:02d}T"
+        f"{t.tm_hour:02d}:{t.tm_min:02d}:{t.tm_sec:02d}"
+    )
 
     return ts
 
 
 def eval_payload_meta(obj):
-    """Recursively iterate through payload metadata and return dictionary with the same structure with callables replaced by their return values."""
+    """Create a measurement payload based on payload metadata.
+
+    Recursively iterates through payload metadata and return dictionary with
+    the same structure with callables replaced by their return values.
+
+    Args:
+        obj (dict | Callable): Sub-dictionary or callable to be evaluated.
+
+    Returns:
+        dict | Any: Sub-dictionary with callables replaced with return values,
+            or the return value of a callable.
+    """
     if isinstance(obj, dict):
         return {key: eval_payload_meta(val) for key, val in obj.items()}
 
